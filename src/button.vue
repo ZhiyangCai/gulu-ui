5a78e1071ba265b5ae394ccd5bb37e7b4c647289
zhiyangcai <2313120974@qq.com>
2020-07-20 17:51:11 +0800

    更新 button 的 icon 样式

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,56 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+      <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+       <div class="content">
+           <slot></slot>
+
+       </div>
+    </button>
 </template>
 
 <script lang="ts">
     export default {
-        name: 'button'
+        name: 'button',
+        props:{
+            icon:{},
+            iconPosition:{
+            type: String,
+                default: 'left',
+                validator(value){
+                    return value==='left'||value==='right'
+                }
+            }
+        }
     };
 </script>
 
 <style lang="scss">
+.g-button{
+    font-size: var(--font-size);
+    height:var(--button-height);
+    padding:0 1em;
+    border-radius: var( --border-radius);
+    border:1px solid  var(--border-color);
+    background: var(--button-bg);
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: middle;
+    a:hover{
+        border-color:var(--border-color-hover)
+}
+    a:active{
+       background-color:var(--button-active-bg)
+   }  a:focus{
+          outline: none;
+      }
+    >.content{order:2}
+    >.icon{order:1;margin-right:.1em;}
+&.icon-right{
+    >.content{order:1}
+    >.icon{order:2;margin-right:0;margin-left:.1em;}
+}
 
+
+}
 </style>
