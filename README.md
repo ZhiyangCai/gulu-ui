9f7f5bc619f9a8e09064c05137343357b87ad02d
zhiyangcai <2313120974@qq.com>
2020-07-20 11:14:02 +0800

    加入parcel, 将button 组件改为单文件组件

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 轱辘 一个Vue UI组件
   作者：czy
+## 安装
+使用本框架前，请在 css 中开启 border-box
+
+```
+*{box-sizing: border-box}
+```
---
+++ src/button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'button'
+    };
+</script>
+
+<style lang="scss">
+
+</style>
